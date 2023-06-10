fc04dc48e6c991bdf0d10046aea8362d0d4f8aba
Dario Russi <113150618+dariorussi@users.noreply.github.com>
2023-06-10 10:15:29 -0500

    Rework gas logic

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -10,7 +10,7 @@ use tracing::{info, warn};
 
 /// The minimum and maximum protocol versions supported by this build.
 const MIN_PROTOCOL_VERSION: u64 = 1;
-const MAX_PROTOCOL_VERSION: u64 = 17;
+const MAX_PROTOCOL_VERSION: u64 = 18;
 
 // Record history of protocol version allocations here:
 //
@@ -52,6 +52,11 @@ const MAX_PROTOCOL_VERSION: u64 = 17;
 //             effects; this also allows us to stop including wrapped tombstones in accumulator.
 //             Add self-matching prevention for deepbook.
 // Version 17: Introduce execution layer versioning, preserve all existing behaviour in v0.
+//             Gas minimum charges moved to be a multiplier over the reference gas price. In this
+//             protocol version the multiplier is the same as the lowest bucket of computation
+//             such that the minimum transaction cost is the same as the minimum computation
+//             bucket.
+//             Add a feature flag to indicate the changes semantics of `base_tx_cost_fixed`
 
 #[derive(Copy, Clone, Debug, Hash, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ProtocolVersion(u64);
@@ -217,7 +222,6 @@ struct FeatureFlags {
     // Enable zklogin auth
     #[serde(skip_serializing_if = "is_false")]
     zklogin_auth: bool,
-
     // How we order transactions coming out of consensus before sending to execution.
     #[serde(skip_serializing_if = "ConsensusTransactionOrdering::is_none")]
     consensus_transaction_ordering: ConsensusTransactionOrdering,
@@ -231,6 +235,10 @@ struct FeatureFlags {
     // regardless of their previous state in the store.
     #[serde(skip_serializing_if = "is_false")]
     simplified_unwrap_then_delete: bool,
+
+    // If true minimum txn charge is a multiplier of the gas price
+    #[serde(skip_serializing_if = "is_false")]
+    txn_base_cost_as_multiplier: bool,
 }
 
 fn is_false(b: &bool) -> bool {
@@ -447,7 +455,6 @@ pub struct ProtocolConfig {
     object_runtime_max_num_store_entries_system_tx: Option<u64>,
 
     // === Execution gas costs ====
-    // note: Option<per-instruction and native function gas costs live in the sui-cost-tables crate
     /// Base cost for any Sui transaction
     base_tx_cost_fixed: Option<u64>,
 
@@ -797,6 +804,10 @@ impl ProtocolConfig {
 
     pub fn simplified_unwrap_then_delete(&self) -> bool {
         self.feature_flags.simplified_unwrap_then_delete
+    }
+
+    pub fn txn_base_cost_as_multiplier(&self) -> bool {
+        self.feature_flags.txn_base_cost_as_multiplier
     }
 }
 
@@ -1177,6 +1188,10 @@ impl ProtocolConfig {
                 cfg.feature_flags.package_upgrades = true;
                 cfg
             }
+            // This is the first protocol version currently possible.
+            // Mainnet starts with version 4. Previous versions are pre mainnet and have
+            // all been wiped out.
+            // Every other chain is after version 4.
             4 => {
                 let mut cfg = Self::get_for_version_impl(version - 1, chain);
                 // Change reward slashing rate to 100%.
@@ -1278,6 +1293,13 @@ impl ProtocolConfig {
                 // cfg.feature_flags.loaded_child_objects_fixed = true;
                 // cfg.feature_flags.ban_entry_init = true;
                 // cfg.feature_flags.pack_digest_hash_modules = true;
+                cfg
+            }
+            18 => {
+                let mut cfg = Self::get_for_version_impl(version - 1, chain);
+                cfg.feature_flags.txn_base_cost_as_multiplier = true;
+                // this is a multiplier of the gas price
+                cfg.base_tx_cost_fixed = Some(1_000);
                 cfg
             }
             // Use this template when making changes:
